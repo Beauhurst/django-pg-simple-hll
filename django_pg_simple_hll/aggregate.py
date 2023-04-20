96930775cecdcd9cf5d5d707bd735f9d63c18b6a
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2023-04-20 15:36:44 +0100

    Add Django Aggregate scaffolding

--- django_pg_simple_hll/aggregate.py
+++ django_pg_simple_hll/aggregate.py
@@ -3,5 +3,7 @@ from django.db.models import Aggregate, IntegerField
 
 class ApproxCardinality(Aggregate):
     function = "hll_approx_cardinality"
+    name = "ApproxCardinality"
     allow_distinct = False
     output_field = IntegerField()
+    empty_result_set_value = 0
