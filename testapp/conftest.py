59ab35771e4abafca36ac7b1fd26873f2c0fd2ab
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2023-04-25 12:02:23 +0100

    Test via python reference implementation instead

--- testapp/conftest.py
+++ testapp/conftest.py
@@ -1,12 +1,32 @@
-from datetime import timedelta
-from random import choice, randint
+from collections.abc import Iterable
+from datetime import datetime, timedelta
+from random import randint
 from typing import Any
-from uuid import uuid4
+from uuid import UUID
 
 import pytest
 from django.utils.timezone import now
 
-from .models import Session
+from .models import Group, Session
+
+CREATED_NOW = now().replace(hour=0, minute=0, second=0, microsecond=0)
+N_USER_IDS = 140_000
+N_DAYS = 7
+
+DB_BATCH_SIZE = 1_000
+
+
+def yield_sessions_for_ids(
+    ids: list[UUID], group: Group, on_day: datetime
+) -> Iterable[Session]:
+    for user_int, user_uuid in enumerate(ids):
+        yield Session(
+            user_uuid=user_uuid,
+            user_int=user_int,
+            user_str=f"{user_int}-{user_uuid}",
+            created=on_day + timedelta(hours=randint(0, 23), minutes=randint(0, 59)),
+            group=group,
+        )
 
 
 @pytest.fixture(scope="session")
@@ -16,38 +36,30 @@ def django_db_setup(  # noqa: PT004 - this fixture's name is fixed by django-pyt
     django_db_setup: Any,
     django_db_blocker: Any,
 ) -> None:
-    created_now = now()
-    n_user_ids = 150_000
-    user_ids = [uuid4() for _ in range(n_user_ids)]
-    sessions = []
-    for _ in range(250_000):
-        user_int = randint(0, n_user_ids - 1)
-        user_uuid = user_ids[user_int]
-        user_str = f"{user_int}-{user_uuid}"
-        timestamp = created_now + timedelta(
-            days=choice([0, 1, 2, 3, 4, 7, 8, 9, 10, 11])
-        )
-        sessions.append(
-            Session(
-                user_uuid=user_uuid,
-                user_int=user_int,
-                user_str=user_str,
-                created=timestamp,
-            )
-        )
+    """
+    Creates a list of users and inserts sessions for those users for each day of the week.
 
-    for _ in range(20_000):
-        user_int = randint(0, n_user_ids - 1)
-        user_uuid = user_ids[user_int]
-        user_str = f"{user_int}-{user_uuid}"
-        timestamp = created_now + timedelta(days=choice([5, 6, 12, 13]))
-        sessions.append(
-            Session(
-                user_uuid=user_uuid,
-                user_int=user_int,
-                user_str=user_str,
-                created=timestamp,
-            )
-        )
+    This is a fixture because it's expensive to create the data, and we want to reuse it
+
+    The first day it will insert sessions for 1/N_DAYS of the users
+    For every day after that, it will insert sessions for a further 1/N_DAYS of users
+    and all previous users
+    """
+
+    user_ids = [UUID(int=i) for i in range(N_USER_IDS)]
+
     with django_db_blocker.unblock():
-        Session.objects.bulk_create(sessions)
+        for day_of_week in range(N_DAYS + 1):
+            group = Group.objects.create(
+                id=UUID(int=day_of_week),
+                created=CREATED_NOW + timedelta(days=day_of_week),
+            )
+
+            Session.objects.bulk_create(
+                yield_sessions_for_ids(
+                    ids=user_ids[: int(day_of_week * (N_USER_IDS / N_DAYS))],
+                    group=group,
+                    on_day=CREATED_NOW + timedelta(days=day_of_week),
+                ),
+                batch_size=DB_BATCH_SIZE,
+            )
--- testapp/tests.py
+++ testapp/tests.py
@@ -1,27 +1,27 @@
-from collections import defaultdict
+import json
 from collections.abc import Iterable
-from math import log, sqrt
+from datetime import timedelta
+from pathlib import Path
 
 import pytest
-from django.db.models import Count
+from django.core.exceptions import FieldError
+from django.db.models import Case, F, Func, Q, When
 from django.db.models.functions import TruncDate
+from django.db.utils import ProgrammingError
 from django_pg_simple_hll.aggregate import ApproxCardinality
 
-from .models import Session
+from .conftest import CREATED_NOW, N_DAYS
+from .hyperloglog import HyperLogLog
+from .models import Group, Session
 
 FIELDS = ("user_int", "user_uuid", "user_str")
 PRECISIONS_TO_TEST = (5, 8, 9, 10, 11, 12)  # if tails intermittently with 4
 
+FIXTURES_DIR = Path(__file__).parent / "fixtures"
 
-def _calculate_error(precision: int) -> float:
-    """
-    Calculates the expected error for the given precision
 
-    E.g. If we use 9 bits of precision, we have 512 buckets
-    we should have sqrt(3 * log(2) - 1) / sqrt(512)
-    -> 4.592% expected error
-    """
-    return sqrt(3 * log(2) - 1) / sqrt(2**precision)
+class HashText(Func):
+    template = "HASHTEXT(%(expressions)s::text) & ~(1 << 31)"
 
 
 def __combine_params(
@@ -32,30 +32,125 @@ def __combine_params(
             yield (field_name, precision_value)
 
 
+def _get_reference_approximation(field: str, precision: int) -> dict[int, int]:
+    """
+    Get the reference approximation for a given field and precision
+    Calculate values only once, and store them in a JSON file for future use
+    """
+    fixture_path = FIXTURES_DIR / f"{field}.{precision}.json"
+
+    if fixture_path.exists():
+        with open(fixture_path) as f:
+            return {int(key): val for key, val in json.load(f).items()}
+
+    fixtures = {}
+    for day_of_week in range(N_DAYS):
+        timestamp = CREATED_NOW + timedelta(days=day_of_week + 2)
+        query = (
+            Session.objects.filter(created__lt=timestamp)
+            .annotate(hash=HashText(F(field)))
+            .values("hash")
+        )
+
+        hll = HyperLogLog(precision)
+        for s in query:
+            hll.add(s["hash"])
+
+        fixtures[day_of_week] = int(hll.cardinality())
+
+    with open(FIXTURES_DIR / f"{field}.{precision}.json", "w") as f:
+        json.dump(fixtures, f)
+
+    return fixtures
+
+
 @pytest.mark.parametrize(
     ("field", "precision"),
     __combine_params(field=FIELDS, precision=PRECISIONS_TO_TEST),
 )
 @pytest.mark.django_db()
 def test_total(field: str, precision: int) -> None:
-    N = 10
-    # get N random records:
-    rand_sessions = list(Session.objects.values_list(field)[:N])
+    fixtures = _get_reference_approximation(field, precision)
+    aggregation = Session.objects.aggregate(
+        approx_unique_users=ApproxCardinality(field, precision),
+    )
 
-    square_difference_of_means = []
-    for session in rand_sessions:
-        filter = {f"{field}__lte": session[0]}
-        aggregation = Session.objects.filter(**filter).aggregate(
-            unique_users=Count(field, distinct=True),
-            approx_unique_users=ApproxCardinality(field, precision),
+    assert fixtures[N_DAYS - 1] == aggregation["approx_unique_users"]
+
+
+@pytest.mark.django_db()
+def test_raises_error_without_expression() -> None:
+    """Aggregation cannot be used without targetting a specific field"""
+    with pytest.raises(ProgrammingError):
+        Session.objects.aggregate(
+            approx_unique_users=ApproxCardinality(),
         )
-        square_difference_of_means.append(
-            (aggregation["unique_users"] - aggregation["approx_unique_users"]) ** 2
+
+
+@pytest.mark.django_db()
+def test_raises_error_with_star() -> None:
+    """Aggregation cannot be used without targetting a specific field"""
+    with pytest.raises(FieldError):
+        Session.objects.aggregate(
+            approx_unique_users=ApproxCardinality("*"),
         )
 
-    actual_error = sqrt(sum(square_difference_of_means) / N)
 
-    assert actual_error <= _calculate_error(precision)
+@pytest.mark.parametrize(
+    ("field", "precision"),
+    __combine_params(field=FIELDS, precision=PRECISIONS_TO_TEST),
+)
+@pytest.mark.django_db()
+def test_with_filter(field: str, precision: int) -> None:
+    days_for_filter = 2
+    fixtures = _get_reference_approximation(field, precision)
+
+    aggregation = Session.objects.aggregate(
+        approx_unique_users=ApproxCardinality(
+            field,
+            precision,
+            filter=Q(created__lt=CREATED_NOW + timedelta(days=days_for_filter + 2)),
+        ),
+    )
+    assert fixtures[days_for_filter] == aggregation["approx_unique_users"]
+
+
+@pytest.mark.parametrize(
+    ("field", "precision"),
+    __combine_params(field=FIELDS, precision=PRECISIONS_TO_TEST),
+)
+@pytest.mark.django_db()
+def test_implicit_join(field: str, precision: int) -> None:
+    fixtures = _get_reference_approximation(field, precision)
+    days_for_filter = 4
+
+    aggregation = Group.objects.filter(
+        created__lt=CREATED_NOW + timedelta(days=days_for_filter + 2)
+    ).aggregate(
+        approx_unique_users=ApproxCardinality(f"sessions__{field}", precision),
+    )
+    assert fixtures[days_for_filter] == aggregation["approx_unique_users"]
+
+
+@pytest.mark.parametrize(
+    ("field", "precision"),
+    __combine_params(field=FIELDS, precision=PRECISIONS_TO_TEST),
+)
+@pytest.mark.django_db()
+def test_with_expression(field: str, precision: int) -> None:
+    fixtures = _get_reference_approximation(field, precision)
+    days_for_expression = 3
+
+    expression = Case(
+        When(
+            created__lt=CREATED_NOW + timedelta(days=days_for_expression + 2),
+            then=F(field),
+        )
+    )  # Works since NULLs aren't counted
+    aggregation = Session.objects.aggregate(
+        approx_unique_users=ApproxCardinality(expression, precision),
+    )
+    assert fixtures[days_for_expression] == aggregation["approx_unique_users"]
 
 
 @pytest.mark.parametrize(
@@ -63,29 +158,16 @@ def test_total(field: str, precision: int) -> None:
 )
 @pytest.mark.django_db()
 def test_by_date(field: str, precision: int) -> None:
-    N = 10
-    # get N random records:
-    rand_sessions = list(Session.objects.values_list(field)[:N])
+    fixtures = _get_reference_approximation(field, precision)
 
-    square_difference_of_means = defaultdict(list)
-    for session in rand_sessions:
-        filter = {f"{field}__lte": session[0]}
-
-        aggregation = (
-            Session.objects.filter(**filter)
-            .annotate(date_of_session=TruncDate("created"))
-            .values("date_of_session")
-            .annotate(
-                approx_unique_users=ApproxCardinality(field, precision),
-                unique_users=Count(field, distinct=True),
-            )
-            .values("approx_unique_users", "unique_users", "date_of_session")
+    aggregation = (
+        Session.objects.annotate(date_of_session=TruncDate("created"))
+        .values("date_of_session")
+        .annotate(
+            approx_unique_users=ApproxCardinality(field, precision),
         )
-        for row in aggregation:
-            square_difference_of_means[row["date_of_session"]].append(
-                (row["unique_users"] - row["approx_unique_users"]) ** 2
-            )
-    expected_error = _calculate_error(precision)
-    for _, square_difference_of_means_for_date in square_difference_of_means.items():
-        actual_error = sqrt(sum(square_difference_of_means_for_date) / N)
-        assert actual_error <= expected_error
+        .values("approx_unique_users", "date_of_session")
+        .order_by("date_of_session")
+    )
+    for i, row in enumerate(aggregation):
+        assert fixtures[i] == row["approx_unique_users"]
