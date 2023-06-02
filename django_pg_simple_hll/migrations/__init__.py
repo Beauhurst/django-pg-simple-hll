ef8189f23c40a6630aca5da6678e2d3456260b47
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2023-06-02 23:14:16 +0100

    use python 3.9 compatible typing

--- django_pg_simple_hll/migrations/__init__.py
+++ django_pg_simple_hll/migrations/__init__.py
@@ -1,8 +1,9 @@
 from os import PathLike
 from pathlib import Path
+from typing import Union
 
 
-def load_sql(path: PathLike[str] | str, reverse: bool = False) -> str:
+def load_sql(path: Union[PathLike[str], str], reverse: bool = False) -> str:
     path = Path(path)
     parent = path.parent
     stem = path.stem
